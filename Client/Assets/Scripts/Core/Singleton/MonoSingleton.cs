de1c4bb0e5d89e6443802818f01dac542272dd78
Jason <489580010@qq.com>
2018-03-09 15:01:58 +0800

    实现泛型单例模式

---
+++ Client/Assets/Scripts/Core/Singleton/MonoSingleton.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T _instance;
+    private static bool isInited = false;
+
+    private void Awake()
+    {
+        if (MonoSingleton<T>._instance == null)
+        {
+            MonoSingleton<T>._instance = this as T;
+        }
+
+        if (!isInited)
+        {
+            MonoSingleton<T>._instance.Initialize();
+            isInited = true;
+        }
+    }
+
+    protected virtual void Initialize()
+    {
+    }
+
+    protected virtual void Dispose()
+    {
+    }
+
+    protected void OnApplicationQuit()
+    {
+        if (null != MonoSingleton<T>._instance)
+        {
+            MonoSingleton<T>._instance.Dispose();
+            MonoSingleton<T>._instance = null;
+            isInited = false;
+        }
+    }
+
+    public static T Reference
+    {
+        get { return _instance; }
+    }
+
+    public static T Instance
+    {
+        get { return GetInstance(); }
+    }
+
+    public static T GetInstance()
+    {
+        if (MonoSingleton<T>._instance == null)
+        {
+            MonoSingleton<T>._instance = UnityEngine.Object.FindObjectOfType(typeof(T)) as T;
+            if (MonoSingleton<T>._instance == null)
+            {                
+                System.Type[] components = new System.Type[] { typeof(T) };
+                MonoSingleton<T>._instance = new GameObject(typeof(T).ToString(), components).GetComponent<T>();
+                MonoSingleton<T>._instance.Initialize();
+                isInited = true;
+            }
+        }
+        return MonoSingleton<T>._instance;
+    }
+}
---
+++ Client/Assets/Scripts/Core/Singleton/Singleton.cs
@@ -0,0 +1,37 @@
+﻿public class Singleton<T> where T : Singleton<T>, new()
+{
+    private static T _instance;
+
+    private static readonly object _sync = new object();
+
+    static Singleton()
+    {
+        Singleton<T>._instance = default(T);
+    }
+
+    public static T Instance
+    {
+        get { return GetInstance(); }
+    }
+
+    public static T GetInstance()
+    {
+        if (null == Singleton<T>._instance)
+        {
+            lock (_sync)
+            {
+                if (null == Singleton<T>._instance)
+                {
+                    Singleton<T>._instance = new T();
+                    Singleton<T>._instance.Initialize();
+                }
+            }
+        }
+        return Singleton<T>._instance;
+    }
+
+    protected virtual void Initialize()
+    {
+
+    }
+}
