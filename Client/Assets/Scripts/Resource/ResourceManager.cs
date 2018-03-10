14bf2070621fb4d63700310f3221e5c71f1f7eb7
jxfcwys <489580010@qq.com>
2018-03-10 11:29:45 +0800

    资源管理器

---
+++ Client/Assets/Scripts/Resource/ResourceManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : Singleton<ResourceManager>
+{
+    //protected ResourceLoader resLoader = null;
+    protected ResourcePool resPool = null;
+
+    protected override void Initialize()
+    {
+        resPool = new ResourcePool();
+    }
+}
---
+++ Client/Assets/Scripts/Resource/ResourcePool.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 资源缓存池
+/// </summary>
+public class ResourcePool
+{
+    protected Dictionary<string, UnityEngine.Object> dicCacheAssets;
+    protected Dictionary<int, string> dicGuidKeys;
+
+    public ResourcePool()
+    {
+        dicCacheAssets = new Dictionary<string, UnityEngine.Object>();
+        dicGuidKeys = new Dictionary<int, string>();
+    }
+
+    public void AddCacheAsset(string path, UnityEngine.Object obj)
+    {
+        if (string.IsNullOrEmpty(path) || null == obj) {
+            return;
+        }
+        if (dicCacheAssets.ContainsKey(path)) {
+            return;
+        }
+        dicCacheAssets.Add(path, obj);
+        dicGuidKeys.Add(obj.GetInstanceID(), path);
+    }
+
+    public void RemoveCacheAsset(UnityEngine.Object obj)
+    {
+        if (null == obj) {
+            return;
+        }
+        int guid = obj.GetInstanceID();
+        string path = string.Empty;
+        if (dicGuidKeys.TryGetValue(guid, out path))
+        {
+            dicCacheAssets.Remove(path);
+            dicGuidKeys.Remove(guid);
+        }
+    }
+
+    public UnityEngine.Object GetCacheAsset(string path)
+    {
+        if (string.IsNullOrEmpty(path)) {
+            return null;
+        }
+        UnityEngine.Object obj;
+        if (dicCacheAssets.TryGetValue(path, out obj)) {
+            return obj;
+        }
+        return null;
+    }
+}
